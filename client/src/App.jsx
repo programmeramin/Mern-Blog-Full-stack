e0f68b0d6acbb99d5a7d2849768b0d1225cd45b1
Programmeramin <developeramin2615@gmail.com>
2025-03-22 16:40:44 +0600

    install react-router-dom and setup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css'
+import Home from "./pages/Home";
+import About from "./pages/About"
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Project from './pages/Project';
 
 function App() {
   
   return (
     <>
-      <h1 className='text-3xl text-red-500'>Hello world</h1>
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/sign-in' element={<SignIn/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/project' element={<Project/>}/>
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }     

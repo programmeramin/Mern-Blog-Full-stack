d7f14e97fb3ad8516bfbcd628aafaad7ba016559
Programmeramin <developeramin2615@gmail.com>
2025-04-16 15:53:31 +0600

    create and add footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,29 +1,67 @@
-import { Footer } from 'flowbite-react'
+import {
+  Footer,
+  FooterBrand,
+  FooterCopyright,
+  FooterDivider,
+  FooterIcon,
+  FooterLink,
+  FooterLinkGroup,
+  FooterTitle,
+} from "flowbite-react";
+import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const FooterComponent = () => {
   return (
     <>
-      <Footer className='border border-t-8 border-teal-500'>
-        <div className="">
-          <div className="">
-            <div className="">
-             <Link to="/"   className='font-bold dark:text-white text-4xl'>
-             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>AMIN'S</span>
+   <Footer container className="border-t-8 border-teal-500">
+      <div className="w-full">
+        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+          <div className="mb-7">
+          <Link to="/"   className='font-bold dark:text-white text-3xl'>
+          <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>AMIN'S</span>
           BLOG
         </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <FooterTitle title="about" />
+              <FooterLinkGroup col>
+                <FooterLink href="#">Flowbite</FooterLink>
+                <FooterLink href="#">Tailwind CSS</FooterLink>
+              </FooterLinkGroup>
             </div>
-
-              <div className="grid grid-cols-2 gap-3 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-                <Footer.Title/>
-
-              </div>
-
+            <div>
+              <FooterTitle title="Follow us" />
+              <FooterLinkGroup col>
+                <FooterLink href="#">Github</FooterLink>
+                <FooterLink href="#">Discord</FooterLink>
+              </FooterLinkGroup>
+            </div>
+            <div>
+              <FooterTitle title="Legal" />
+              <FooterLinkGroup col>
+                <FooterLink href="#">Privacy Policy</FooterLink>
+                <FooterLink href="#">Terms &amp; Conditions</FooterLink>
+              </FooterLinkGroup>
+            </div>
           </div>
         </div>
-           
-      </Footer>
+        <FooterDivider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <FooterCopyright href="#" by="Amin's Blog" year={new Date().getFullYear()} />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <FooterIcon href="#" icon={BsFacebook} />
+            <FooterIcon href="#" icon={BsInstagram} />
+            <FooterIcon href="#" icon={BsTwitter} />
+            <FooterIcon href="#" icon={BsGithub} />
+            <FooterIcon href="#" icon={BsDribbble} />
+          </div>
+        </div>
+      </div>
+    </Footer>
     </>
   )
 }

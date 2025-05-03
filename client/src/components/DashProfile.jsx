d16fbc66120bd0fc6eb7dcf9f2a2a5f605a699a7
Programmeramin <developeramin2615@gmail.com>
2025-05-03 15:46:55 +0600

    Dashprofile ui completed

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,32 +1,103 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { Button, TextInput } from 'flowbite-react';
-
+import { getStorage } from "firebase/storage";
+import { Alert, Button, TextInput } from "flowbite-react";
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { app } from "./../firebase";
 const DashProfile = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const filePickerRef = useRef();
 
-    const {currentUser} = useSelector(state => state.user)
+  const handleImageChange = (e) => {
+    // e.preventDefault();
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile?.name;
+    const storageRef = useRef(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError(
+          `Could not upload image (File must be less than 2MB `
+        );
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        });
+      }
+    );
+  };
 
   return (
-    <div className='max-w-lg mx-auto p-3 w-full'>
-      <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-       <from className="flex flex-col gap-4">
-          <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+      <from className="flex flex-col gap-4">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
 
-          <img src={currentUser?.profilePicture} alt="" className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'/>
-          </div>
-           <TextInput id='username' placeholder='username' defaultValue={currentUser?.username}/>
-           <TextInput id='email' placeholder='email' defaultValue={currentUser.email}/>
-           <TextInput id='password' placeholder='password'/>
-            <Button type='submit' gradientDuoTone="purpleToBlue" outline>
-              Update
-            </Button>
-       </from>
-        <div className="text-red-500 flex justify-between mt-5">
-          <span>DeleteAccount</span>
-          <span className='cursor-pointer'>Sign Out</span>
+        <div
+          className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          onClick={() => filePickerRef.current.click()}
+        >
+          <img
+            src={imageFileUrl || currentUser?.profilePicture}
+            alt=""
+            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+          />
         </div>
-    </div>
-  )
-}
+        {imageFileUploadError && (
+          <Alert color="failure">{imageFileUploadError}</Alert>
+        )}
 
-export default DashProfile
+        <TextInput
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser?.username}
+        />
+        <TextInput
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.email}
+        />
+        <TextInput id="password" placeholder="password" />
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
+          Update
+        </Button>
+      </from>
+      <div className="text-red-500 flex justify-between mt-5">
+        <span>DeleteAccount</span>
+        <span className="cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
+};
+
+export default DashProfile;
